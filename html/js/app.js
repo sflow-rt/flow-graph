1619431dbb588ae97f776c6b2a1027491d332cdf
sflow-rt <peter.phaal@gmail.com>
2015-09-28 21:36:06 -0700

    Initial commit

---
+++ html/js/app.js
@@ -0,0 +1,93 @@
+$(function() {
+  var linksURL = '../scripts/graph.js/links/json';
+  var hosts = {};
+  var links = {};
+  var nodes;
+  var edges;
+  var network;
+
+  function addLink(link,from,to) {
+    links[link] = link;
+    edges.add({
+      id:link,
+      from:from,
+      to:to 
+    });
+  }
+  function removeLink(link) {
+    delete links[link];
+    edges.remove({id:link}); 
+  }
+  function addHost(host,group) {
+    hosts[host] = host;
+    nodes.add({
+      id: host,
+      label: host,
+      group:group
+    });
+  }
+  function removeHost(host) {
+    delete hosts[host];
+    nodes.remove({id:host}); 
+  }
+  function updateLinks(data) {
+    var new_hosts = {};
+    var new_links = {};
+    for(var link in data) {
+      var info = data[link];
+      new_hosts[info.start] = info.start;
+      new_hosts[info.end] = info.end;
+      new_links[link] = link;
+      if(!hosts[info.start]) addHost(info.start,info.grpstart);
+      if(!hosts[info.end]) addHost(info.end,info.grpend);
+      if(!links[link]) addLink(link,info.start,info.end);
+    }
+    for(var link in links) {
+      if(!new_links[link]) removeLink(link);
+    }
+    for(var host in hosts) {
+      if(!new_hosts[host]) removeHost(host);
+    }
+  } 
+
+  var timeout_poll;
+  function pollLinks() {
+    $.ajax({
+       url:linksURL,
+       dataType:'json',
+       success: function(data) {
+         updateLinks(data);
+         timeout_poll = setTimeout(pollLinks, 2000);
+       },
+       error: function(result,status,errorThrown) {
+         timeout_poll = setTimeout(pollLinks, 2000);
+       },
+       timeout: 60000
+     }); 
+  };
+
+  $(document).ready(function() {
+    nodes = new vis.DataSet([]);
+    edges = new vis.DataSet([]);
+
+    var container = document.getElementById('mynetwork');
+
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {
+      physics:{
+        solver:'repulsion'
+      },
+      groups: {
+        external: {color:{background:'#FF6666'}},
+        private: {color:{background:'#33CC33'}},
+        multicast: {color:{background:'#FFFF00'}}
+      }
+    };
+    network = new vis.Network(container, data, options);
+
+    pollLinks();
+  });
+});
---
+++ scripts/graph.js
@@ -0,0 +1,63 @@
+setGroups('flowgraph', {
+  external:['0.0.0.0/0','::/0'],
+  private:['10.0.0.0/8','172.16.0.0/12','192.168.0.0/16','FC00::/7'],
+  multicast:['224.0.0.0/4']
+});
+
+var links = {};
+var agingtime = 600000;
+
+setIntervalHandler(function() {
+  var now = (new Date()).getTime();
+  for (var link in links) {
+    var info = links[link];
+    if(now - info.time > agingtime) {
+      delete links[link];
+    }
+  }
+});
+
+setFlowHandler(function(rec) {
+  var [src,dst,grpsrc,grpdst] = rec.flowKeys.split(',');
+  var link;
+  var start;
+  var grpstart;
+  var end;
+  var grpend;
+  if(src < dst) {
+    link = src+'-'+dst;
+    start = src;
+    end = dst;
+    grpstart = grpsrc;
+    grpend = grpdst;
+  } else {
+    link = dst+'-'+src;
+    start = dst;
+    end = src;
+    grpstart = grpdst;
+    grpend = grpsrc;
+  }
+  links[link] = {
+    time:rec.start,
+    start:start,end:end,
+    grpstart:grpstart,grpend:grpend
+  };
+},['flowgraph-pair']);
+
+setHttpHandler(function(req) {
+  var result, key, name, path = req.path;
+  if(!path || path.length === 0) throw "not_found";
+  switch(path[0]) {
+  case 'links':
+    result = links;
+    break;
+  default: throw 'not_found';
+  }
+  return result;  
+});
+
+setFlow('flowgraph-pair',{
+  keys:'ipsource,ipdestination,group:ipsource:flowgraph,group:ipdestination:flowgraph',
+  value:'frames',
+  log:true,flowStart:true
+});
